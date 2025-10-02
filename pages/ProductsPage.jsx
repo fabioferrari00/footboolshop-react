af5b1e86e083bb77320468a50cff81f42633c0ff
SergioGBosco <sergiogibosco@gmail.com>
2025-10-02 16:59:05 +0200

    aggiunta Pagina prodotti BOZZA

--- pages/ProductsPage.jsx
+++ pages/ProductsPage.jsx
@@ -2,8 +2,48 @@ import React from 'react'
 
 const ProductsPage = () => {
   return (
-    <div>
-      <h1>Prodotti</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 d-flex justify-content-center my-4">
+          <h1>Un ampio catalogo a tua Disposizione!</h1>
+        </div>
+
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-6 col-lg-4">
+          <div className="card">
+            <img src="https://calcioitalia.com/media/cache/sylius_shop_product_large_thumbnail/1c/76/801e75123c83d43cdcdc15f20b78.jpeg" className="card-img-top" alt="Product 1" />
+            <div className="card-body">
+              <h5 className="card-title">Scarpe da Calcio</h5>
+              <p className="card-text">Le migliori scarpe per il tuo gioco.</p>
+              <a href="#" className="btn btn-primary">Aggiungi Al Carrello</a>
+            </div>
+          </div>
+
+        </div>
+        <div className="col-12 col-md-6 col-lg-4">
+          <div className="card">
+            <img src="https://assets.goal.com/images/v3/blt749df1fb5a388269/GOAL_-_Multiple_Images_-_2_Split_-_Facebook_(68).jpg?auto=webp&format=pjpg&width=3840&quality=60" className="card-img-top" alt="Product 1" />
+            <div className="card-body">
+              <h5 className="card-title">Scarpe da Calcio</h5>
+              <p className="card-text">Le migliori scarpe per il tuo gioco.</p>
+              <a href="#" className="btn btn-primary">Aggiungi Al Carrello</a>
+            </div>
+          </div>
+
+        </div>
+        <div className="col-12 col-md-6 col-lg-4">
+          <div className="card">
+            <img src="https://store.inter.it/images/inter/products/large/IN25A05.webp" className="card-img-top" alt="Product 1" />
+            <div className="card-body">
+              <h5 className="card-title">Scarpe da Calcio</h5>
+              <p className="card-text">Le migliori scarpe per il tuo gioco.</p>
+              <a href="#" className="btn btn-primary">Aggiungi Al Carrello</a>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
