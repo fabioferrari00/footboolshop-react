0aa4c6cc4bad009f977c9e92d66b70c7cad5f028
Manuel-Lauriola <manuelauriol@gmail.com>
2025-10-02 16:51:22 +0200

    [UPD] - footer :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <h1>Iniiziamo!</h1>
 
+      <Footer />
     </>
   )
 }
