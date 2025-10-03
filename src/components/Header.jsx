5abb87de05339dd57adc2ea81db266fdc4656c0d
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-10-03 12:31:12 +0200

    commit sul main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,29 +4,26 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header>
-
-      <NavLink>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <div className='w-150'>
-              <img className='logo' src="/footboolshop_logo.png" alt="Logo" />
-            </div>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <NavLink className="navbar-nav">
-                <Link className="nav-item nav-link" aria-current="page" to={`/`}>Home</Link>
-                <Link className="nav-item nav-link" to={`/products`}>Prodotti</Link>
-                <Link className="nav-item nav-link" to={`/contacts`}>Contatti</Link>
-                <Link className="nav-item nav-link" to={`/about_us`}>Chi siamo</Link>
-              </NavLink>
-            </div>
+    <header className="navbar navbar-expand-lg">
+      <div className="container-fluid d-flex justify-content-between">
+        <div className='d-flex align-items-center'>
+          <img className='logo' src="/footboolshop_logo.png" alt="Logo" />
+          <h2>FootBoolShop</h2>
+        </div>
+        <div>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <NavLink className="navbar-nav">
+              <Link className="nav-item nav-link" aria-current="page" to={`/`}>Home</Link>
+              <Link className="nav-item nav-link" to={`/products`}>Prodotti</Link>
+              <Link className="nav-item nav-link" to={`/contacts`}>Contatti</Link>
+              <Link className="nav-item nav-link" to={`/about_us`}>Chi siamo</Link>
+            </NavLink>
           </div>
-        </nav>
-      </NavLink>
-
+        </div>
+      </div>
     </header>
   )
 }
