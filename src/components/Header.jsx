668a0b87e2cfab0f00cad8295f71ed56766e3d50
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-10-03 11:48:17 +0200

    giochiamo con l'header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,39 +5,28 @@ import { Link, NavLink } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="header-btns col-12 d-flex justify-content-between me-3 mt-3">
-            <img className='logo' src="./public/footboolshop_logo.png" alt="Logo" />
-            <NavLink>
-              <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                  <a class="navbar-brand" href="#">Navbar</a>
-                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-                  <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                      <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </nav>
-            </NavLink>
+
+      <NavLink>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <div className='w-150'>
+              <img className='logo' src="/footboolshop_logo.png" alt="Logo" />
+            </div>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <NavLink className="navbar-nav">
+                <Link className="nav-item nav-link" aria-current="page" to={`/`}>Home</Link>
+                <Link className="nav-item nav-link" to={`/products`}>Prodotti</Link>
+                <Link className="nav-item nav-link" to={`/contacts`}>Contatti</Link>
+                <Link className="nav-item nav-link" to={`/about_us`}>Chi siamo</Link>
+              </NavLink>
+            </div>
           </div>
-        </div>
-      </div>
+        </nav>
+      </NavLink>
+
     </header>
   )
 }
