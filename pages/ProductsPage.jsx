dce8f6710e2a483c28da08ba54662c40f521c5a0
SergioGBosco <sergiogibosco@gmail.com>
2025-10-06 11:39:28 +0200

    edit-card

--- pages/ProductsPage.jsx
+++ pages/ProductsPage.jsx
@@ -28,8 +28,8 @@ const ProductsPage = () => {
         {products.map((product) => {
           if (product.team_name == "Inter")
             return (
-              <div className="col-12 col-md-6 col-lg-4  noDecoration" >
-                <Link to={`/product/${product.slug}`}>
+              <div className="col-12 col-md-6 col-lg-4  noDecoration" key={product.id} >
+                <Link to={`/product/${product.slug}`} state={{ id: product.id }} >
                   <div className="card ">
                     <img src={product.image_url} className="card-img-top" alt="Product 1" />
 
@@ -42,14 +42,15 @@ const ProductsPage = () => {
               </div>
             )
         })}
+
       </div>
       <div className="row gy-3  my-4">
         <h2 className='section_team' >Milan</h2>
         {products.map((product) => {
           if (product.team_name == "Milan")
             return (
-              <div className="col-12 col-md-6 col-lg-4  noDecoration" >
-                <Link to={`/product/${product.slug}`}>
+              <div className="col-12 col-md-6 col-lg-4  noDecoration" key={product.id} >
+                <Link to={`/product/${product.slug}`} state={{ id: product.id }} >
                   <div className="card ">
                     <img src={product.image_url} className="card-img-top" alt="Product 1" />
 
@@ -68,8 +69,8 @@ const ProductsPage = () => {
         {products.map((product) => {
           if (product.team_name == "Roma")
             return (
-              <div className="col-12 col-md-6 col-lg-4  noDecoration" >
-                <Link to={`/product/${product.slug}`}>
+              <div className="col-12 col-md-6 col-lg-4  noDecoration" key={product.id} >
+                <Link to={`/product/${product.slug}`} state={{ id: product.id }} >
                   <div className="card ">
                     <img src={product.image_url} className="card-img-top" alt="Product 1" />
 
@@ -88,8 +89,8 @@ const ProductsPage = () => {
         {products.map((product) => {
           if (product.team_name == "Cagliari")
             return (
-              <div className="col-12 col-md-6 col-lg-4 noDecoration" >
-                <Link to={`/product/${product.slug}`}>
+              <div className="col-12 col-md-6 col-lg-4 noDecoration" key={product.id} >
+                <Link to={`/product/${product.slug}`} state={{ id: product.id }} >
                   <div className="card ">
                     <img src={product.image_url} className="card-img-top" alt="Product 1" />
 
@@ -108,8 +109,8 @@ const ProductsPage = () => {
         {products.map((product) => {
           if (product.team_name == "Juventus")
             return (
-              <div className="col-12 col-md-6 col-lg-4 noDecoration " >
-                <Link to={`/product/${product.slug}`}>
+              <div className="col-12 col-md-6 col-lg-4 noDecoration " key={product.id} >
+                <Link to={`/product/${product.slug}`} state={{ id: product.id }} >
                   <div className="card ">
                     <img src={product.image_url} className="card-img-top" alt="Product 1" />
                     <div className="card-body">
@@ -127,8 +128,8 @@ const ProductsPage = () => {
         {products.map((product) => {
           if (product.team_name == "Napoli")
             return (
-              <div className="col-12 col-md-6 col-lg-4 noDecoration" >
-                <Link to={`/product/${product.slug}`}>
+              <div className="col-12 col-md-6 col-lg-4 noDecoration" key={product.id} >
+                <Link to={`/product/${product.slug}`} state={{ id: product.id }} >
                   <div className="card ">
                     <img src={product.image_url} className="card-img-top" alt="Product 1" />
 
