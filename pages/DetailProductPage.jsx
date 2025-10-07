30f5d50abc5de4c63ec59016301907979aca8f26
espindolasol95 <espindolasol.ok@gmail.com>
2025-10-07 23:43:07 +0200

    [test] rimozione button

--- pages/DetailProductPage.jsx
+++ pages/DetailProductPage.jsx
@@ -47,7 +47,7 @@ const DetailProductPage = () => {
 
           <div className='mt-4'>
             <button className='btn btn-primary me-2'>Aggiungi al carrellino</button>
-            <button className='btn btn-primary me-2'>Preferiti</button>
+            
           </div>
         </div>
       </div>
