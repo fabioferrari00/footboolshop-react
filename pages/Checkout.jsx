6753db7c0a74224863448abd174b0dbadb113b81
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-10-10 13:39:57 +0200

    Funziona invio email e responsive HERO modifica file .env, rimozione template del venditore aggiunta template di emailjs per invio codice sconto

--- pages/Checkout.jsx
+++ pages/Checkout.jsx
@@ -90,14 +90,6 @@ export default function CheckoutPage() {
         items_list,
       };
 
-      //Invia al proprietario
-      await emailjs.send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-        emailData,
-        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
-      );
-
       //Invia al cliente (nuovo template)
       await emailjs.send(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
